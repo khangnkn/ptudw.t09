57d1435cc1145285608adc7cfc549f9966be2bde
Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2019-04-15 00:03:58 +0700

    Redefine the structure of project

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+// const Handlebars = require('handlebars');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', {title: 'User'})
 });
 
+router.get('/login', (req, res) => {
+  res.render('login', {title: 'Dang nhap'});
+})
 module.exports = router;
