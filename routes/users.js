8f19784746e7eab197fd6dbee2e88b3f8d34a6e9
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-04-15 22:35:34 +0700

    Change password page

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // const Handlebars = require('handlebars');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'User'})
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "User" });
 });
 
-router.get('/login', (req, res) => {
-  res.render('login', {title: 'Dang nhap'});
-})
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Dang nhap" });
+});
+
+router.get("/changepassword", (req, res) => {
+  res.render("changepassword", { title: "Dang nhap" });
+});
 module.exports = router;
