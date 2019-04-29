51cf947939dc628aea24d1a25cafcb7ceed0733a
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-04-29 09:23:25 +0700

    ...

--- routes/writer.js
+++ routes/writer.js
@@ -3,38 +3,35 @@ var router = express.Router();
 
 const sidedata = [
   {
-    href: '/writer/editor',
-    src: '/images/icons/baseline-create-24px.svg',
-    content: 'Viết bài'
+    href: "/writer/editor",
+    src: "/images/icons/baseline-create-24px.svg",
+    content: "Viết bài",
   },
   {
-    href: '/writer/articles',
-    src: '/images/icons/baseline-format_align_right-24px.svg',
-    content: 'Các bài đã đăng'
-  }
-]
+    href: "/writer/articles",
+    src: "/images/icons/baseline-format_align_right-24px.svg",
+    content: "Các bài đã đăng",
+  },
+];
 
 router.get("/", function(req, res) {
-  res.redirect('/writer/welcome')
+  res.redirect("/writer/welcome");
 });
 
 router.get("/welcome", function(req, res) {
   res.render("writer-welcome", {
     title: "Chào mừng, phóng viên!",
     extra: '<link rel="stylesheet" href="/stylesheets/writer.css">',
-    sideitem: sidedata});
-})
+    sideitem: sidedata,
+  });
+});
 
 router.get("/editor", function(req, res) {
   res.render("writer-new-post", {
     title: "Biên tập bài viết",
-<<<<<<< HEAD
     extra:
-      '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /><link rel="stylesheet" href="/stylesheets/custom-theme.css"> <link rel="stylesheet" href="/stylesheets/writer.css">',
-=======
-    extra: '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /><link rel="stylesheet" href="/stylesheets/custom-theme.css"><link rel="stylesheet" href="/stylesheets/writer.css">',
-    sideitem: sidedata
->>>>>>> origin/master
+      '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /><link rel="stylesheet" href="/stylesheets/custom-theme.css"><link rel="stylesheet" href="/stylesheets/writer.css">',
+    sideitem: sidedata,
   });
 });
 
@@ -42,7 +39,7 @@ router.get("/articles", function(req, res) {
   res.render("writer-show-articles", {
     title: "Các bài đã viết",
     extra: '<link rel="stylesheet" href="/stylesheets/writer.css">',
-    sideitem: sidedata
+    sideitem: sidedata,
   });
 });
 
