aa416c7b87b38f06a2faba65739a74e3b414f276
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-04-30 08:23:55 +0700

    Fixed #21

--- routes/editor.js
+++ routes/editor.js
@@ -2,14 +2,16 @@
 var express = require("express");
 var router = express.Router();
 
+var badge = { draft: 0, publish: 0 };
+
 /* GET users listing. */
 router.get("/drafts", function(req, res) {
   res.render("editor-drafts", {
     title: "Blog Posts",
     extra:
       '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> </script><link rel="stylesheet" href="/stylesheets/editor.css">',
-    draft: 9,
-    publish: 10,
+    draft: badge.draft,
+    publish: badge.publish,
   });
 });
 
