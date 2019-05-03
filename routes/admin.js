0c72924428d0456ca28c28766a36400288200dc5
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-05-03 19:18:19 +0700

    trivial

--- routes/admin.js
+++ routes/admin.js
@@ -49,4 +49,12 @@ router.get("/drafts", function(req, res) {
   });
 });
 
+router.get("/users", function(req, res) {
+  res.render("admin-users", {
+    title: "Quản lý người dùng",
+    extra:
+      '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> <link rel="stylesheet" href="/stylesheets/admin.css">',
+  });
+});
+
 module.exports = router;
