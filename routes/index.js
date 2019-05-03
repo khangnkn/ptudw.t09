b166325c0a8afd91384d3fb31ced7ee2f2e52885
Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2019-05-04 00:01:12 +0700

    Home page implementation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('general-index', { title: 'Express', body: '<p>Hello<p>' });
+router.get("/", function(req, res, next) {
+  res.render("general-index", {
+    title: "Express",
+    body: "<p>Hello<p>",
+    extra: '<link rel="stylesheet" href="/stylesheets/home.css">'
+  });
 });
 
 module.exports = router;
