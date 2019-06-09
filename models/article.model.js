5c221a5ff78c25092a518bd28d74973732bab02a
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-06-09 23:48:50 +0700

    basically done with writer/editor

--- models/article.model.js
+++ models/article.model.js
@@ -1,8 +1,12 @@
-var db = require('../utils/db.util');
+var db = require("../utils/db.util");
 
 module.exports = {
-    GetTop4: () => {
-        var sql = "select Id, Title, Author, Date, Category, Cover, Abstract from Articles limit 1";
-        return db.load(sql)
-    }
-}
+  GetTop4: () => {
+    var sql =
+      "select Id, Title, Author, Date, Category, Cover, Abstract from Articles limit 1";
+    return db.load(sql);
+  },
+  add: article => {
+    return db.insert("drafts", article);
+  },
+};
--- routes/writer.js
+++ routes/writer.js
@@ -1,5 +1,7 @@
 var express = require("express");
+const articlemodule = require("../models/article.model");
 var router = express.Router();
+var moment = require("moment");
 
 const sidedata = [
   {
@@ -35,6 +37,32 @@ router.get("/editor", function(req, res) {
       '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /><link rel="stylesheet" href="/stylesheets/custom-theme.css"><link rel="stylesheet" href="/stylesheets/writer.css">',
     sideitem: sidedata,
   });
+});
+
+router.post("/editor", (req, res, next) => {
+  // req.body["State"] = 1;
+  // req.body["Category"] = 1;
+  // req.body["Date"] = "2019-1-1";
+  // req.body["Cover"] = "hasdfsdf";
+  // req.body["Abstract"] = "hi";
+  // debugger;
+  var obj = {
+    Title: req.body["Title"],
+    Category: 1,
+    Date: moment()
+      .format("YYYY-DD-MM")
+      .toString(),
+    Cover: null,
+    Author: 1,
+    Content: req.body["Content"],
+    Abstract: "hi",
+    State: 1,
+  };
+  console.log(obj);
+  articlemodule
+    .add(obj)
+    .then(res.end("done"))
+    .catch(next);
 });
 
 router.get("/articles", function(req, res) {
