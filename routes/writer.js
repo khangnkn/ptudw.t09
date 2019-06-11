33eb5162a057d4fd857163c9a0338b182b628403
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-06-11 23:06:03 +0700

    Correcten some details

--- routes/writer.js
+++ routes/writer.js
@@ -3,7 +3,8 @@ const articlemodule = require("../models/article.model");
 var router = express.Router();
 var moment = require("moment");
 
-const sidedata = [{
+const sidedata = [
+  {
     href: "/writer/editor",
     src: "/images/icons/baseline-create-24px.svg",
     content: "Viết bài",
@@ -15,11 +16,11 @@ const sidedata = [{
   },
 ];
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   res.redirect("/writer/welcome");
 });
 
-router.get("/welcome", function (req, res) {
+router.get("/welcome", function(req, res) {
   res.render("writer/writer-welcome", {
     layout: "writer-layout",
     title: "Chào mừng, phóng viên!",
@@ -28,13 +29,14 @@ router.get("/welcome", function (req, res) {
   });
 });
 
-router.get("/:id/editor", function (req, res) {
+router.get("/:id/editor", function(req, res) {
   var _id = req.param.id;
   res.render("writer/writer-new-post", {
     id: _id,
     layout: "writer-layout",
     title: "Biên tập bài viết",
-    extra: '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /><link rel="stylesheet" href="/stylesheets/custom-theme.css"><link rel="stylesheet" href="/stylesheets/writer.css">',
+    extra:
+      '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /><link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /><link rel="stylesheet" href="/stylesheets/custom-theme.css"><link rel="stylesheet" href="/stylesheets/writer.css">',
     sideitem: sidedata,
   });
 });
@@ -55,11 +57,11 @@ router.post("/:id/editor", (req, res, next) => {
   console.log(obj);
   articlemodule
     .add(obj)
-    .then(res.end("done"))
+    .then(console.log("OK"))
     .catch(next);
 });
 
-router.get("/articles", function (req, res) {
+router.get("/articles", function(req, res) {
   res.render("writer/writer-show-articles", {
     layout: "writer-layout",
     title: "Các bài đã viết",
--- utils/db.util.js
+++ utils/db.util.js
@@ -1,24 +1,32 @@
 var mysql = require("mysql");
+// var createConnection = () => {
+//   return mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "techhub_v3",
+//   });
+// };
+
 var createConnection = () => {
   return mysql.createConnection({
-    host: 'localhost',
+    host: "db4free.net",
     port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'techhub_v3',
+    user: "techhub",
+    password: "ptudw.t09",
+    database: "techhub",
   });
-
-}
+};
 module.exports = {
   load: sql => {
     return new Promise((resolve, reject) => {
       var connection = createConnection();
       connection.connect();
       connection.query(sql, (error, results, fields) => {
-        if (error)
-          reject(error);
-        resolve(results)
-      })
+        if (error) reject(error);
+        resolve(results);
+      });
       connection.end();
     });
   },
@@ -26,13 +34,12 @@ module.exports = {
   insert: (tableName, obj) => {
     return new Promise((resolve, reject) => {
       var connection = createConnection();
-      var sql = `INSERT INTO ${tableName} set ?`
+      var sql = `INSERT INTO ${tableName} set ?`;
       connection.connect();
       connection.query(sql, obj, (error, results, fields) => {
-        if (error)
-          reject(error);
-        resolve(results)
-      })
+        if (error) reject(error);
+        resolve(results);
+      });
       connection.end();
     });
   },
@@ -80,30 +87,19 @@ module.exports = {
         resolve(results.affectedRows);
       });
 
-      <<
-      <<
-      <<
-      < HEAD
+      // <<
+      // <<
+      // <<
+      // < HEAD
       connection.end();
     });
   },
-}; ===
-===
-=
-connection.end();
-});
-},
-
-executeNoReturn: sql => {
-  var connection = createConnection();
-  connection.connect();
-  connection.query(sql, (error, results, fields) => {
-    if (error)
-      console.log(error);
-  })
-  connection.end();
-}
-} >>>
->>>
->
-feature / top
+  executeNoReturn: sql => {
+    var connection = createConnection();
+    connection.connect();
+    connection.query(sql, (error, results, fields) => {
+      if (error) console.log(error);
+    });
+    connection.end();
+  },
+};
